8f01253846d5182acdb9b473454006c576ef023a
Maciej Gajewski <gajewski.mm@gmail.com>
2014-09-24 13:16:09 +0100

    Use existing has_doi? method

--- lib/orcid_profile.rb
+++ lib/orcid_profile.rb
@@ -4,14 +4,17 @@ class OrcidProfile
 
   class Work
     attr_reader :title, :doi
+
     def initialize element
       @title = element.css("title").text
-      @doi = element.css("work-external-identifier-id").text
+      @doi   = element.css("work-external-identifier-id").text
       scrape_crossref unless has_doi?
     end
+
     def altmetric_article
-      @altmetric_article ||= AltmetricArticle.new(self) if @doi.present?
+      @altmetric_article ||= AltmetricArticle.new(self) if has_doi?
     end
+
     def has_doi?
       @doi.present?
     end
@@ -28,8 +31,8 @@ class OrcidProfile
   attr_reader :name, :id, :works
 
   def initialize orcid_id
-    @id = orcid_id
-    @name = fetch.css('credit-name').text
+    @id    = orcid_id
+    @name  = fetch.css('credit-name').text
     @works = fetch('orcid-works').xpath('//xmlns:orcid-work').map { |n| Work.new(n) }
   end
 
@@ -38,13 +41,13 @@ class OrcidProfile
 
   def fetch path = ""
     full_path = make_uri(path)
-    key = "orcid.api_cache.#{ full_path }"
-    data = $redis.get(key) || open(full_path).read
+    key       = "orcid.api_cache.#{ full_path }"
+    data      = $redis.get(key) || open(full_path).read
     $redis.setex key, 1.hour.to_i, data
     Nokogiri::XML data
   end
 
- def make_uri path
+  def make_uri path
     "#{ORCID_API_BASE_URL}/#{@id}/#{path}"
   end
 
